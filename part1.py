93662685ea9f7458b607cf571758369c1e46ac8a
myname21-meet <myname21@meet.mit.edu>
2019-07-28 09:17:27 -0400

    .

--- part1.py
+++ part1.py
@@ -1,13 +1,21 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
-	pass
+	return "hello_world"
 
 def greet_by_name(name):
-	pass
+
+	return "hello" +name 
+
 
 def encode(x):
-	pass
+	return x*3953531
 
 def decode(y):
-	pass
+	return y/3953531.0
+
+a = 5
+a = encode(a)
+print(a)
+a = decode(a)
+print(a)
--- part2.py
+++ part2.py
@@ -1,7 +1,11 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	if y not in range(1,250) and x not in range(500,1000):
+		print("Invalid input: Outside range.")
+		return None
 
 def decode(coded_message):
 	pass
+
+encode(2,1200)
